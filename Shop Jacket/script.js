cef6610331592761d39b12dfb3ae47f0dca9ff7c
kumar1310aryan <kumar1310aryan@gmail.com>
2024-09-03 22:44:02 +0530

    shop jacket

--- Shop Jacket/script.js
+++ Shop Jacket/script.js
@@ -1,21 +1,21 @@
-gsap.from([".heading", "nav"], {
-  x: 1000,
-  opacity: 0,
-  duration: 1.5,
-  delay: 0.3,
-  stagger: 0.2,
-});
+// gsap.from([".heading", "nav"], {
+//   x: 1000,
+//   opacity: 0,
+//   duration: 1.5,
+//   delay: 0.3,
+//   stagger: 0.2,
+// });
 
-gsap.from(".exclusive", {
-  x: -1000,
-  opacity: 0,
-  duration: 1.5,
-  delay: 0.3,
-});
+// gsap.from(".exclusive", {
+//   x: -1000,
+//   opacity: 0,
+//   duration: 1.5,
+//   delay: 0.3,
+// });
 
-gsap.from(".addToCart, .image", {
-  opacity: 0,
-  duration: 1.5,
-  delay: 1,
-  stagger: 0.3,
-});
+// gsap.from(".addToCart, .image", {
+//   opacity: 0,
+//   duration: 1.5,
+//   delay: 1,
+//   stagger: 0.3,
+// });
