cbc404a14821ce657d8ebf6f9ae04d622b52d535
kumar1310aryan <kumar1310aryan@gmail.com>
2024-09-10 17:04:14 +0530

    cursor and page1

--- sheriyansCodingSchool/OBYS/script.js
+++ sheriyansCodingSchool/OBYS/script.js
@@ -48,4 +48,15 @@ function loadingAnimation() {
   });
 }
 
+function cursorAnimation() {
+  document.addEventListener("mousemove", function (dets) {
+    gsap.to("#crsr", {
+      left: dets.x,
+      top: dets.y,
+    });
+  });
+
+  Shery.makeMagnet("#nav-part2 h4");
+}
 loadingAnimation();
+cursorAnimation();
