c27d0d4be2ae97a743bdddf9165516340ac7c2a5
kumar1310aryan <kumar1310aryan@gmail.com>
2024-09-10 22:30:07 +0530

    pagee1 animations

--- sheriyansCodingSchool/OBYS/script.js
+++ sheriyansCodingSchool/OBYS/script.js
@@ -46,6 +46,15 @@ function loadingAnimation() {
   tl.to("#loader", {
     display: "none",
   });
+
+  tl.from("#nav", {
+    opacity: 0,
+  });
+
+  tl.from("#hero1 h1, #hero2 h1, #hero3 h2,#hero3 h3, #hero4 h1", {
+    y: 120,
+    stagger: 0.2,
+  });
 }
 
 function cursorAnimation() {
