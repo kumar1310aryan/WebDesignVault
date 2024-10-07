ee1d78fae5bb6710d8d3f5681657cedc3c6818e1
kumar1310aryan <kumar1310aryan@gmail.com>
2024-10-07 20:16:27 +0530

    quberto shery

--- Quberto/script.js
+++ Quberto/script.js
@@ -1,3 +1,39 @@
+Shery.mouseFollower();
+Shery.makeMagnet(".magnet");
+Shery.hoverWithMediaCircle(".hover", {
+  videos: ["./assets/1.mp4", "./assets/2.mp4", "./assets/3.mp4"],
+});
+
+gsap.to(".left-element", {
+  scrollTrigger: {
+    trigger: "#featured-contents",
+    pin: true,
+    start: "top top",
+    end: "bottom bottom",
+    endTrigger: ".last",
+    scrub: 1,
+  },
+  y: "-400%",
+  ease: Power1,
+});
+
+let elements = document.querySelectorAll(".left-element");
+Shery.imageEffect(".images", {
+  style: 2,
+  slideStyle: (setScroll) => {
+    elements.forEach(function (elem, index) {
+      ScrollTrigger.create({
+        trigger: elem,
+        start: "top top",
+        scrub: 1,
+        onUpdate: function (prog) {
+          setScroll(prog.progress + index);
+        },
+      });
+    });
+  },
+});
+
 function newInspoScrollAnimation() {
   let lastScrollY = window.scrollY; // Store the initial scroll position
   const heading = document.querySelector("#new-heading-wrapper"); // Select the wrapper
